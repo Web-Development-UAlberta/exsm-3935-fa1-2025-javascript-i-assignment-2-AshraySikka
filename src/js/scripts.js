60f40dc1ccd3fd6680c745cc67017d1d7ccef483
Ashray Sikka <ashraysikka@Ashrays-MacBook-Air.local>
2025-11-05 12:27:20 -0500

    Initial commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,51 @@
-// eslint-disable-next-line no-unused-vars
-/* global output, input */
-// eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
+  let cost_of_goods = await input("Please enter the total cost of goods: $");
+  let cash_payment = await input("Please enter the amount of cash payment by the customer: $");
+  let discount_code = Number(await input("Please enter the discount code: "));
+  
+  let discount = 0;
+  let balance = 0;
+  let error = "no";
+  
+  output(" "); // Adding a new line
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+  switch(discount_code){
+    case 0:
+            discount = 0;
+            break;
+    case 1:
+            discount = 10;
+            break;
+    case 2:
+            discount = 15;
+            break;
+    case 3:
+            discount = 25;
+            break;
+    case 4:
+            discount = 35;
+            break;
+    case 5:
+            discount = 40;
+            break;
+    default:
+            error = "yes";
+  }
+  
+  cost_of_goods = cost_of_goods - (cost_of_goods*(discount/100));
+  balance = Math.round((cash_payment - cost_of_goods) * 100) / 100; 
+  // ^ rounding the balance to 2 decimal places by moving the decimal by 2 places and then rounding and then moving the decimal back
+  
+  if (error == "yes"){
+    output("Error! Invalid Discount code.");
+  }
+  else if (balance == 0){
+    output("Balance is $"+balance+", no change to be returned!");
+  }
+  else if (balance < 0){
+    output("Insufficient Payment! Customer needs to pay $"+Math.abs(balance)+" more."); // using absolute function to remove the negative sign
+  }
+  else{
+    output("Successful Payment! Please give $"+balance+" back to the customer.");
   }
 }
